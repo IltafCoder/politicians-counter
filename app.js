fe11075d89f3609ea9dc96d85868369f30c5ddb3
Iltaf Khan <138275083+IltafCoder@users.noreply.github.com>
2026-03-22 22:38:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3003
+
+// set EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// serve static files (optional)
+app.use(express.static("public"));
+
+// HOME (optional)
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+// 🔥 PERSON ROUTE (MAIN PART)
+app.get("/person", (req, res) => {
+  const name = req.query.name;
+  const value = Number(req.query.value);
+
+  if (!name || !value) {
+    return res.send("Invalid data!");
+  }
+
+  res.render("person", { name, value });
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+});
